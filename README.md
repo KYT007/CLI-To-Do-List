c28e2d632ded99e14fee4e305722bb3c169f8fd1
Kyle <yeakelkylet@gmail.com>
2023-11-19 10:56:03 -0500

    SimpleCRUD Prototype updated to fix errors and add functionality

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ The program contained in this repository will be a C.R.U.D. type app I will be c
 I continue to learn more about the python programming language
 and software engineering and development in general. This application will start out simple and be updates as I learn more;
 As I learn databases, how to implement GUIs this program will grow and change.
+
+Update for SimpleCRUD.py 11/19/2023
+
+***Fixed many errors with the base framework of the protoype program.
+***Input validations using pyip were not implemented where they should have been. 
+***Items were not being properly appended to storage as class objects as they were inteded to be.
+
+read_item() function updated for errors, title() attribute was unneeded. 
+
--- SimpleCRUD.py
+++ SimpleCRUD.py
@@ -11,36 +11,55 @@ class ToDoItems:
 
 
 storage = [] # for now, we will use this to store our to do list items. Later on,
-# a database will be used.
+# a database will be used. Its important to note this program is only a prototype.
 
 def main_menu(): #Displays all menu options.
-    print("Main Menu \n. (1.) Make Note \n (2.) Delete Note \n (3.) Edit Note")
+    print("Main Menu \n. (A.) Make Note \n (B.) Delete Note \n (C.) Edit Note")
 
 def run():  #Primary function that starts the program and allows the user to choose options
     # more options and functionalities TBA.
     while True:
         main_menu()
         print("Please select an option: ")
-        choice = input()
+        choice = pyip.inputChoice(['A', 'B', 'C'])
         if choice == "A":
             write_in()
         elif choice == "B":
             read_item()
 
 def write_in():
+    # For this section, we prompt the user for input asigned to variables,
+    # these variables become the attributes of our class instance.
+    # We then assign the instance to the variable "item" and append to storage.
+    
     while True:
-        tbe = input("Please Enter an item: ").title()
-        storage.append(tbe)
+        title = pyip.inputStr("Please Enter a title: ")
+        description = pyip.inputStr("Please Enter a description: ")
+        due_date = pyip.inputStr("Please Enter a due date: ")
+        item = ToDoItems(title, description, due_date)
+        storage.append(item)
         print("Entry Saved!")
         time.sleep(0.5)
         another_entry = pyip.inputChoice(["Yes", "No"], prompt="Do you want to add another item? (Yes/No): ")
         if another_entry == "No":
-            break  # Exit the loop if the user chooses "No"
+            break
+
     
 
-
-
-
-
 def read_item():
+    while True:
+        for item in storage:
+            print("\nTitle:", item.Title)
+            print("Description:", item.Description)
+            print("Due Date:", item.Due_Date +"\n")
+        if not storage:
+            print("Looks like your storage is empty!")
+
+        print("What would you like to do?\n(A.) Main Menu\n(B.) Edit")
+        RRchoice = pyip.inputChoice(["A", "B"])
+        if RRchoice == "A":
+            break 
+def edit_item():
     pass
+
+run()
