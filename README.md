1cb0fa39d4824552c60574e565e030e8f3e30049
Kyle <yeakelkylet@gmail.com>
2023-12-07 10:30:34 -0500

    Made updates to the simple crud program, fixed multiple issues.

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
+# A Simple C.R.U.D. Program Using Pyinputplus for Input Validation
+
 The program contained in this repository will be a C.R.U.D. type app I will be creating and continously adding to as I continue to learn more about the python programming language
-and software engineering. This application will start out simple and be updates as I expand my knowledebase.
+and software engineering. This application will start out simple and be updated as I expand my knowledebase.
 As I learn the usage of databases, how to implement GUIs and similar concepts, this program will grow and change.
+
 
 Update for SimpleCRUD.py 11/19/2023
 
-***Fixed many errors with the base framework of the protoype program.
-***Input validations using pyip were not implemented where they should have been. 
-***Items were not being properly appended to storage as class objects as they were inteded to be.
+# Fixed Issues.
+
+Detected and fixed many errors with the base framework of the protoype program.
+Input validations using pyip were not implemented where they should have been. 
+Items were not being properly appended to storage as class objects as they were inteded to be.
 
 read_item() function updated for errors, title() attribute was unneeded. 
 
+12/4/2023, 12/7/2023.
+# Features added. 
+Edit item function was added in. Read and write_in functions updated to suit this development.
+Delete_item() functionality stage set.
+
+## Fixed multiple issues.
+Fixed multiple positional arguments issues.
+".Title" in line 77, letter T was not capitalized, throwing an error. 
+Replaced some variable names with more sensible (and less nonsensical) ones.
+
+## Bad Design in Flow Control error discovered from exiting edit function.
+The flow control of this program is flawed from the maker's perspective. Current possibilities for refactoring being considered and will be implemented by the next update.
+
+##### Fixed spelling errors in the readme.
+
+Those were embarassing. 
--- SimpleCRUD.py
+++ SimpleCRUD.py
@@ -23,13 +23,13 @@ def run():  #Primary function that starts the program and allows the user to cho
         print("Please select an option: ")
         choice = pyip.inputChoice(['A', 'B', 'C'])
         if choice == "A":
-            write_in()
+            enter_item()
         elif choice == "B":
             read_item()
         elif choice == "C":
-            edit_item(storage)
+            edit_item()
 
-def write_in():
+def enter_item():
     # For this section, we prompt the user for input asigned to variables,
     # these variables become the attributes of our class instance.
     # We then assign the instance to the variable "item" and append to storage.
@@ -40,11 +40,11 @@ def write_in():
         due_date = pyip.inputStr("Please Enter a due date: ")
         item = ToDoItems(title, description, due_date)
         storage.append(item)
-        print("Entry Saved!")
+        print("****" + "Entry Saved!" + "*****")
         time.sleep(0.5)
         another_entry = pyip.inputChoice(["Yes", "No"], prompt="Do you want to add another item? (Yes/No): ")
         if another_entry == "No":
-            break
+            run()
 
     
 
@@ -58,16 +58,38 @@ def read_item():
             print("Looks like your storage is empty!")
 
         print("What would you like to do?\n(A.) Main Menu\n(B.) Edit")
-        RRchoice = pyip.inputChoice(["A", "B"])
-        if RRchoice == "A":
-            break 
-def edit_item(storage):
-    print("Choose an item to edit: ")
+        user_choice = pyip.inputChoice(["A", "B"])
+        if user_choice == "A":
+            break
+        elif user_choice == "B":
+            edit_item()
+#The edit functionality allows the user to edit to-do items in storage. 
+def edit_item():
+    print(f"Please choose an item to edit: ")
     while True:
         if not storage:
             print("looks like there's no items here! ")
             return
         for index, item in enumerate(storage, start=1):
-            print(f"{index}, {item.Description}")
-        indy = pyip.inputStr("Select item: ")
+        # The code above shows us the storage list contents and numbers the items,
+        #starting at 1.
+       
+            print(f"{index}, {item.Title}, {item.Description}, {item.Due_Date}") #here we allow the user to select the item they want to edit.
+        index_slection = pyip.inputInt("Select item: ", min=1, max=len(storage))
+        print(f"You've selected {index_slection}")
+        #This code has the user enter the new information for the item they're editing
+        new_title = pyip.inputStr("Please Enter a title: ")
+        new_description = pyip.inputStr("Please Enter a description: ")
+        new_due_date = pyip.inputStr("Please Enter a due date: ")
+        edited_item = ToDoItems(new_title, new_description, new_due_date)
+        storage[index_slection -1] = edited_item
+        print(f"****" + "Item updated!" + "*****")
+        time.sleep(0.3)
+        run()
+
+def delete_item():
+    pass
+
+        
+        
 run()
